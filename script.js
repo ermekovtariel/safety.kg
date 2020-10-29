124fb9b7983ddd9b09de95637deea7d2119562ab
Tariel <62035728+ermekovtariel@users.noreply.github.com>
2020-10-30 01:36:00 +0600

    'safe.kg_disinfaction'

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector('.order').addEventListener('click',()=>{
+    document.querySelector('.first').classList.add('show')
+})
+document.querySelector('.desinfection').addEventListener('click',()=>{
+    document.querySelector('.second').classList.add('show')
+})
